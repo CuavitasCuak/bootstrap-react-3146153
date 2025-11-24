42fa34930e8b0319136eddd9e5d3dc3cd9ae1f64
CuavitasCuak <marincuaava@gmail.com>
2025-11-24 13:22:54 -0500

    Nueva Carpeta

---
+++ 04-Visumbrio/src/App.jsx
@@ -0,0 +1,42 @@
+
+import './App.css'
+import { NavBar } from './assets/Componentes/NavBar'
+
+function App() {
+ 
+
+  return (
+    <>
+      
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+
+      <NavBar/>
+    </>
+
+
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default App
---
+++ 04-Visumbrio/src/assets/Componentes/NavBar.jsx
@@ -0,0 +1,8 @@
+
+import React from 'react'
+
+export const NavBar = () => {
+  return (
+    <h1>NavBar</h1>
+  )
+}
