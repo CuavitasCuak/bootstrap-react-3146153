ae8df148122c9edcf3dfb6749a7d4bcd4bb46a9e
CuavitasCuak <marincuaava@gmail.com>
2025-11-25 13:26:25 -0500

    Avance en NavBar

--- 04-Visumbrio/src/assets/Componentes/NavBar.jsx
+++ 04-Visumbrio/src/assets/Componentes/NavBar.jsx
@@ -3,6 +3,51 @@ import React from 'react'
 
 export const NavBar = () => {
   return (
-    <h1>NavBar</h1>
+   <nav className="navbar navbar-expand-lg bg-body-tertiary" style={{width:"100%", height:"7rem"}}>
+  <div className="container-fluid" >
+
+      <div className="logo">
+        <img src="./logooo.png" alt="Logo de la marca" style={{width: "15rem", height: "auto" }} />
+        </div>
+
+  
+
+    <button className="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      
+      <span className="navbar-toggler-icon"></span>
+
+    </button>
+    <div className="collapse navbar-collapse " id="navbarNav">
+
+      <ul className="navbar-nav">
+        
+        <li className="nav-item">
+
+          <a className="nav-link active " aria-current="page" href="#">Inicio</a>
+       
+        </li>
+      
+
+        <li className="nav-item">
+
+          <a className="nav-link" href="#">Cómic</a>
+
+        </li>
+
+        <li className="nav-item">
+
+          <a className="nav-link" href="#">Tienda</a>
+
+        </li>
+
+        <li className="nav-item">
+
+          <a className="nav-link" href="true">Iniciar sesión</a>
+
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
   )
 }
